ffdff6a2be936fc22a6f9a1cba6e1bfb55594308
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-03-11 16:09:54 +0300

    http client

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ToDo } from '../../models/toDo';
 
 @Component({
   standalone: true,
@@ -9,51 +10,34 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   styleUrl: './homepage.component.scss',
 })
 export class HomepageComponent implements OnInit {
-  todoList: string[] = ['Eleman 1'];
+  todoList: ToDo[] = [];
 
   constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
-    //this.getTodos();
-    this.getTodos2();
+    this.getTodos();
   }
 
   getTodos() {
     // backend'e istek atıp verileri çek
-    //this.httpClient.get('https://jsonplaceholder.typicode.com/todos');
-    console.log('Fonksiyon öncesi');
-    this.someAsyncOperation()
-      .then((response: string) => {
-        console.log('Cevap başarılı promiseden şu değer geldi:', response);
-      }) // işlem başarılı
-      .catch((error) => {
-        console.log('Cevap başarısız promiseden şu değer geldi:', error);
-      }) // işlem başarısız
-      .finally(() => {
-        console.log('Promise işlemi başarılı ya da başarısız sonlandı.');
-      }); // işlem bitti (başarılı-başarısız)
-    // then-catch-finally
-    setTimeout(() => {
-      console.log('Fonksiyon sonrası');
-    }, 3001);
-    console.log('Sync');
+    this.httpClient
+      .get<ToDo[]>('https://jsonplaceholder.typicode.com/todos')
+      .subscribe({
+        next: (response: ToDo[]) => {
+          console.log('Backendden cevap geldi:', response);
+          this.todoList = response;
+        },
+        error: (error) => {
+          console.log('Backendden hatalı cevap geldi:', error);
+        },
+        complete: () => {
+          console.log('Backend isteği sonlandı.');
+        },
+      });
   }
 
-  async getTodos2() {
-    try {
-      let value: string = await this.someAsyncOperation();
-      console.log(value);
-    } catch (error) {
-      console.log('Hata:', error);
-    }
-  }
-
-  someAsyncOperation(): Promise<string> {
-    // CB, Callback function
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        reject('123456');
-      }, 3000);
-    });
+  postToDo() {
+    let obj = {};
+    this.httpClient.post('link', obj).subscribe();
   }
 }
