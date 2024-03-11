cda4353e5611223058b25d94de467ffae275f7dc
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-03-11 13:57:38 +0300

    two way data binding

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'myform', // html'de çağrılacak isim
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
 })
-export class FormComponent {}
+export class FormComponent {
+  username: string = '';
+  // Reactive Forms
+
+  writeToConsole() {
+    console.log(this.username);
+  }
+
+  changeFromTs() {
+    this.username = 'örnek';
+  }
+}
 // ngModel araştırma
-// typescriptte interfaceler / modeller
 // two way data binding
+
+// typescriptte interfaceler / modeller
+
+// CLI => Command Line Interface
