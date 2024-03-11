14d64b9a011ae65dcc8ddf9e4c608a478f382f43
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-03-11 15:57:46 +0300

    promise

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,12 +1,59 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss',
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
   todoList: string[] = ['Eleman 1'];
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    //this.getTodos();
+    this.getTodos2();
+  }
+
+  getTodos() {
+    // backend'e istek atıp verileri çek
+    //this.httpClient.get('https://jsonplaceholder.typicode.com/todos');
+    console.log('Fonksiyon öncesi');
+    this.someAsyncOperation()
+      .then((response: string) => {
+        console.log('Cevap başarılı promiseden şu değer geldi:', response);
+      }) // işlem başarılı
+      .catch((error) => {
+        console.log('Cevap başarısız promiseden şu değer geldi:', error);
+      }) // işlem başarısız
+      .finally(() => {
+        console.log('Promise işlemi başarılı ya da başarısız sonlandı.');
+      }); // işlem bitti (başarılı-başarısız)
+    // then-catch-finally
+    setTimeout(() => {
+      console.log('Fonksiyon sonrası');
+    }, 3001);
+    console.log('Sync');
+  }
+
+  async getTodos2() {
+    try {
+      let value: string = await this.someAsyncOperation();
+      console.log(value);
+    } catch (error) {
+      console.log('Hata:', error);
+    }
+  }
+
+  someAsyncOperation(): Promise<string> {
+    // CB, Callback function
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject('123456');
+      }, 3000);
+    });
+  }
 }
