286c7a28b24252ed26fff9edffa7c676eb89797c
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-03-11 15:05:14 +0300

    ngfor & ngif

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss',
 })
-export class HomepageComponent {}
+export class HomepageComponent {
+  todoList: string[] = ['Eleman 1'];
+}
