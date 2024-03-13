868309ab4d1be9c4382f98a94b232b25ce2c1948
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-03-13 14:17:24 +0300

    reactive forms

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,50 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BaseInputComponent } from '../../components/base-input/base-input.component';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, BaseInputComponent],
+  imports: [CommonModule, BaseInputComponent, FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
-export class RegisterComponent {}
+export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.createFormGroup();
+  }
+
+  createFormGroup() {
+    this.registerForm = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(7),
+      ]),
+      passwordConfirm: new FormControl('', [
+        Validators.required,
+        Validators.minLength(7),
+      ]),
+    });
+  }
+
+  submit() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+    } else {
+      // Amatör çözüm
+      alert('Validasyon hatası.');
+    }
+  }
+}
